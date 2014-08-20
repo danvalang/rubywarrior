e977b6c45aed6b8011630bc74562549c283e5627
Daniel Valle <dvalle0@gmail.com>
2014-08-20 00:41:47 -0500

    level 4 done

--- player.rb
+++ player.rb
@@ -1,14 +1,18 @@
 class Player
-  def play_turn(warrior)
-    if warrior.feel.empty?
-    	if (warrior.health < 10)
-    		warrior.rest!
-    	else
-    		warrior.walk!
-    	end
-    else
-    	warrior.attack!
-    end
-
-  end
+	def play_turn(warrior)
+		if warrior.feel.empty?
+			if (warrior.health < 15) 
+				if (warrior.health >= @last_health)
+					warrior.rest!
+				else
+					warrior.walk!
+				end
+			else
+				warrior.walk!
+			end
+		else
+			warrior.attack!
+		end
+		@last_health = warrior.health
+	end
 end
