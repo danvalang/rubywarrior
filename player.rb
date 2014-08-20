b147cc2e38451670171ad82ca449d07e0ad8aa86
Daniel Valle <dvalle0@gmail.com>
2014-08-20 01:34:29 -0500

    level 6 done

--- player.rb
+++ player.rb
@@ -1,20 +1,37 @@
 class Player
+	DANGER_HEALTH = 7
+	MIN_HEALTH = 15
 	def play_turn(warrior)
-		if warrior.feel.empty?
-			if (warrior.health < 15)
-				if (warrior.health >= @last_health)
-					warrior.rest!
-				else
-					warrior.walk!
-				end
+		@last_health ||= warrior.health
+		@took_damage = @last_health > warrior.health
+		@direction ||= :forward
+		
+		if warrior.feel(@direction).empty?
+			if should_flee? warrior
+				@direction = :backward
+				warrior.walk! @direction
+			elsif should_rest? warrior
+				warrior.rest!
 			else
-				warrior.walk!
+				warrior.walk! @direction
 			end
-		elsif warrior.feel.captive?
-			warrior.rescue!
+		elsif warrior.feel(@direction).captive?
+			warrior.rescue!(@direction)
+		elsif warrior.feel(@direction).wall?
+			@direction = :forward
 		else
-			warrior.attack!
+			warrior.attack!(@direction)
 		end
 		@last_health = warrior.health
+	end
+
+	# If warrior should rest
+	def should_rest?(warrior)
+		!@took_damage && warrior.health < MIN_HEALTH
+	end
+	# if warrior should flee
+	def should_flee?(warrior)
+		bad_health = warrior.health < DANGER_HEALTH
+		@took_damage && bad_health
 	end
 end
